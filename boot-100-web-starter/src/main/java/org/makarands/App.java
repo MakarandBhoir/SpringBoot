985ef238e72c5e4a9ceb4549ac66acebadd6d448
MakarandBhoir <makarandbhoir@synergetics-india.com>
2017-06-16 15:11:10 +0530

    Add files via upload

---
+++ boot-100-web-starter/src/main/java/org/makarands/App.java
@@ -0,0 +1,14 @@
+package org.makarands;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(App.class);
+		//app.setBannerMode(Banner.Mode.OFF);
+	    app.run(args);
+	}
+}
